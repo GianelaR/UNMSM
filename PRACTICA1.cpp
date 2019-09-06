3f02362a96b33b9ac1d1275136342a164fd0500e
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:08:23 -0500

    prueba1

---
+++ PRACTICA1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+
+ int main(){
+     int a,b,s;
+     cout<<"Ingresa dos numeros"<<endl;
+     cin>>a>>b;
+     s=a+b;
+     cout<<"LA SUMA ES "<<s<<endl;
+     getch();
+    return 0;
+ }
